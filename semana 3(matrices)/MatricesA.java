5cb62e81fd95f73ca539d8819c0fab4df0b5165b
jhonatan-dev051 <jmachadoa@estudiante.uniajc.edu.co>
2026-09-08 11:25:13 -0500

    corrección

--- semana 3(matrices)/MatricesA.java
+++ semana 3(matrices)/MatricesA.java
@@ -1,6 +1,16 @@
 public class MatricesA {
 
      public static void main(String[] args) {
+
          //Declaracion de la matriz (Forma 1)
-     }
+         int[][] m = {{5,8,6},{1,3,2},{4,9,7}};
+
+         //Recorrido de la matriz
+         for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) { //m[0] hace referencia a las columnas
+                System.out.println("m[" + i + "][" + j +"]=" + m[i][j]);    
+            }
+        }
+       
+    }
 }
