7e6a06c900b75fb23e734ce9f276a028ccf2bc61
jhonatan-dev051 <jmachadoa@estudiante.uniajc.edu.co>
2026-09-08 12:01:52 -0500

    correcion de errores

--- semana 3(matrices)/MatricesA.java
+++ semana 3(matrices)/MatricesA.java
@@ -1,16 +1,24 @@
 public class MatricesA {
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
 
-         //Declaracion de la matriz (Forma 1)
-         int[][] m = {{5,8,6},{1,3,2},{4,9,7}};
+        // Declaracion de la matriz (Forma 1)
+        int[][] m = { { 5, 8, 6 }, { 1, 3, 2 }, { 4, 9, 7 } };
 
-         //Recorrido de la matriz
-         for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[0].length; j++) { //m[0] hace referencia a las columnas
-                System.out.println("m[" + i + "][" + j +"]=" + m[i][j]);    
+        // Recorrido de la matriz
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) { // m[0] hace referencia a las columnas
+                System.out.println("m[" + i + "][" + j + "]=" + m[i][j]);
             }
         }
-       
+//Visualizar de mejor manera la matriz 
+ String cad = "";
+ for (int i = 0; i < m.length; i++) {
+    for (int j = 0; j < m[0].length; j++) {  // m[0] hace referencia a las columnas
+        cad +=  "!" + m[i][j];
+    }
+    cad += "| \n";
+ }
+ System.out.println(cad);
     }
 }
