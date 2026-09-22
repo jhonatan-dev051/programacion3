758a6a5a257be421627e6d25a6c07e3430cbce5a
Elwolfin12 <jlmena@estudiante.uniajc.edu.co>
2026-09-22 13:14:53 -0500

    Prueba

--- parcial1/src/Cine.java
+++ parcial1/src/Cine.java
@@ -1,5 +1,8 @@
 public class Cine {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        
+   private Pelicula[] peliculas;
+ 
     }
 }
